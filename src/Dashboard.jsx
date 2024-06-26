dfc734cc02077b2fda2d8dd182b863745192c030
Ishita <ishitajain385@gmail.com>
2024-06-26 16:45:45 +0530

    new changes

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Dashboard() {
   return (
     <div>
-      <h1>LOGIN OR SIGNUP BEFORE YOU START</h1>
+      <h1>LOGIN OR SIGNUP BEFORE YOU START
+        <Link to="/Login"> LOGIN</Link>
+        <br /> <br />
+        <Link to="/Signup">SIGN UP</Link>
+      </h1>
     </div>
   )
 }
