1c6aad0899467b95d70323560e36e6c84a4714d9
Ishita <ishitajain385@gmail.com>
2024-06-26 17:27:53 +0530

    latest changes

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Dashboard() {
   return (
-    <div>
+    <div style={{display:"flex",flexDirection:"column", alignItems:"center",marginTop:"200px"}}>
       <h1>LOGIN OR SIGNUP BEFORE YOU START
-        <Link to="/Login"> LOGIN</Link>
-        <br /> <br />
-        <Link to="/Signup">SIGN UP</Link>
+      <div style={{marginLeft:"250px"}} > <Link to="/Login"> LOGIN</Link>     </div>  
+        
+      <div  style={{marginLeft:"250px"}}>  <Link to="/Signup">SIGN UP</Link>  </div>  
       </h1>
     </div>
   )
